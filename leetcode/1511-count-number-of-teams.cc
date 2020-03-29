95efe8a0a0d0528151cf9da00020538443f0d5dd
Magic <magic.z@outlook.com>
2020-03-29 10:42:52 +0800

    LeetCode 1511: Count Number of Teams.

---
+++ leetcode/1511-count-number-of-teams.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+      if(rating.size()<3){
+        return 0;
+      }
+      int answer(0);
+      for(int i=0;i<rating.size()-2;i++){
+        for(int j=i+1;j<rating.size()-1;j++){
+          for(int k=j+1;k<rating.size();k++){
+            if((rating[i]<rating[j]&&rating[j]<rating[k])||(rating[i]>rating[j]&&rating[j]>rating[k])){
+              answer++;
+            }
+          }
+        }
+      }
+      return answer;
+    }
+};
