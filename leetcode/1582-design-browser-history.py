7b4cb3ad714f24f189ceb9d950890b285df30ecd
Magic <magic.z@outlook.com>
2020-06-07 11:07:00 +0800

    LeetCode 1582: Design Browser History.

---
+++ leetcode/1582-design-browser-history.py
@@ -0,0 +1,32 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history=[homepage]
+        self.index=0
+        
+
+    def visit(self, url: str) -> None:
+        if self.index<len(self.history)+1:
+            self.history=self.history[:self.index+1]
+        self.history.append(url)
+        self.index+=1
+        
+
+    def back(self, steps: int) -> str:
+        self.index-=steps
+        self.index=max(self.index,0)
+        return self.history[self.index]
+        
+
+    def forward(self, steps: int) -> str:
+        self.index+=steps
+        self.index=min(self.index, len(self.history)-1)
+        return self.history[self.index]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
