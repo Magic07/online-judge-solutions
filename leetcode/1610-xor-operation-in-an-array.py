1411cbff8132cbe983ac5a776a004e297ec0d60a
Magic <magic.z@outlook.com>
2020-06-21 10:33:54 +0800

    LeetCode 1610: XOR Operation in an Array.

---
+++ leetcode/1610-xor-operation-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        answer=start
+        for i in range(1,n):
+            answer=answer^(start+2*i)
+        return answer
+        
+        
