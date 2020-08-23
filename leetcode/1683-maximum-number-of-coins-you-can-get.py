b088a9ce9ea0121ff625919352bb97b5f60c19b4
Magic <magic.z@outlook.com>
2020-08-23 10:51:00 +0800

    LeetCode 1683: Maximum Number of Coins You Can Get.

---
+++ leetcode/1683-maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+      piles=sorted(piles)
+      n=len(piles)
+      answer=0
+      for i in range(int(n/3),n, 2):
+        answer+=piles[i]
+      return answer
+        
